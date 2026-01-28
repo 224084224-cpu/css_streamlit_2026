5620324ac1452ee582142e0a9e115827749bb5bc
224084224-cpu <224084224@edu.vut.ac.za>
2026-01-28 15:10:13 +0200

    Revise text and add markdown in app_one.py
    Updated the text and added a markdown section to the Streamlit app.

--- app_one.py
+++ app_one.py
@@ -10,13 +10,16 @@ import streamlit as st
 
 st.write("CSS 2026  :)")
 
-st.write("day3")
+st.write("Coding summer school day3")
 
 st.title("Tshishonga Vhulenda")
+
+st.markdown("""Ahuna na zwinwe loto chela zwezwi""")
 
 number= st.slider("pick a number",1,100)
 
 st.write(f"you picked {number}")
+
 
 
 
