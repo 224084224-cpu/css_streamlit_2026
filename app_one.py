4fd0c7661358c0c935c60879b42f0eb95a15878a
224084224-cpu <224084224@edu.vut.ac.za>
2026-01-28 21:46:29 +0200

    Format and clean up app_one.py

--- app_one.py
+++ app_one.py
@@ -10,11 +10,12 @@ import streamlit as st
 
 st.write("CSS 2026  :)")
 
-st.write("Coding summer school day3")
+st.write("Coding summer school day 3")
 
 st.title("Tshishonga Vhulenda")
 
 st.markdown("""Ahuna na zwinwe loto Tshela zwezwi""")
+
 
 
 
