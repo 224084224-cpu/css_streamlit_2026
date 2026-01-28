a02a20d6f064ed76bbcbcf850f056266627b44ab
224084224-cpu <224084224@edu.vut.ac.za>
2026-01-28 14:59:20 +0200

    Add files via upload

---
+++ app_one.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 28 09:51:29 2026
+
+@author: MUKWEVHO EVANCE
+"""
+
+import streamlit as st
+
+
+st.write("CSS 2026  :)")
+
+st.write("day3")
+
+st.title("Tshishonga Vhulenda")
+
+number= st.slider("pick a number",1,100)
+
+st.write(f"you picked {number}")
+
+
+
+
