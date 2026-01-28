fccb360837818d4bb0e52ec7952d60113397d592
224084224-cpu <224084224@edu.vut.ac.za>
2026-01-28 22:12:16 +0200

    Refactor profile page layout and add styling
    Updated the layout and styling of the profile page.

--- app_one.py
+++ app_one.py
@@ -5,27 +5,32 @@ Created on Wed Jan 28 09:51:29 2026
 @author: MUKWEVHO EVANCE
 """
 
-import streamlit as st
 
 
 import streamlit as st
 
-# Page config (optional but nice)
+# Page config
 st.set_page_config(
     page_title="Profile",
     layout="centered"
 )
 
-# Add vertical space
+# Vertical spacing
 st.markdown("<br><br><br>", unsafe_allow_html=True)
 
-# Centered heading
+# Rounded rectangle (card)
 st.markdown(
     """
-    <h1 style="text-align: center;">👋 Hello!, I'm Tshishonga Charles</h1>
-    <h3 style="text-align: center; color:  blue;">
-        Welcome to my profile
-    </h3>
+    <div style="
+        background-color: #f9f9f9;
+        padding: 40px;
+        border-radius: 20px;
+        text-align: center;
+        box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
+    ">
+        <h1>👋 Hi, I'm Charles Tshishonga</h1>
+        <h3 style="color: blue;">Welcome to my profile</h3>
+    </div>
     """,
     unsafe_allow_html=True
 )
