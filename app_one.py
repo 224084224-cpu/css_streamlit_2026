45c9a7cf65bfee4470e936fffe16cd4296d2863d
224084224-cpu <224084224@edu.vut.ac.za>
2026-02-02 09:29:44 +0200

    Refactor Streamlit app to use tabs for sections

--- app_one.py
+++ app_one.py
@@ -9,28 +9,31 @@ st.set_page_config(page_title="Profile", layout="centered")
 st.write("")
 st.write("")
 
+tabs = st.tabs(["Profile", "Education", "Contact", "Skills"])
+
 # Card container
-with tabs[1]:        
+with tabs[0]:        
         st.subheader(" Hi, I'm Charles Tshishonga")
         st.markdown(":blue[Welcome to my profile🎓]")
     
         st.divider()
-with tabs[2]:
+with tabs[1]:
         st.subheader("Education")
         st.write("**School📕:** Edison Nesengani Secondary")
         st.write("**Instituition🏛:** Vaal University Of Technology")
         st.write("**Department:** Engineering and Technology")
     
-with tabs[3]:
+with tabs[2]:
         st.subheader("Contact details")
         st.write("**Email address🖨:** vhulendacharles32@gmail.com")
         st.write("**Contact details📲:** +27 66 427 4152")
         st.write("**Instagram**: @Kadosh1644")
 
-with tabs[4]:
+with tabs[3]:
         st.subheader("Other Skills")
         st.write("Python")
         st.write("C++")
+
 
 
 
